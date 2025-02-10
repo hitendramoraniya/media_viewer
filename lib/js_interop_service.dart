abcbbdeb2d2fc32fe54fcbb32c06d0b77d667dfa
hitendra <hitendramoraniya@gmail.com>
2025-02-10 05:46:32 +0530

    update code

---
+++ lib/js_interop_service.dart
@@ -0,0 +1,25 @@
+import 'package:js/js.dart';
+
+@JS()
+external _toggleFullScreen();
+
+@JS()
+external _enterFullScreen();
+
+@JS()
+external _exitFullScreen();
+
+class JsInteropService {
+
+  toggleFullScreen() {
+    _toggleFullScreen();
+  }
+
+  enterFullScreen() {
+    _enterFullScreen();
+  }
+
+  exitFullScreen() {
+    _exitFullScreen();
+  }
+}
